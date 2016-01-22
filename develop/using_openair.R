ad884b6e11c7e895d0b5b64be70cd72d84ac8ae7
masalmon <maelle.salmon@yahoo.se>
2016-01-22 09:23:12 +0100

    comments the file

--- develop/using_openair.R
+++ develop/using_openair.R
@@ -6,6 +6,10 @@ library("openair")
 library("dplyr")
 
 measurementsNL <- NULL
+# get all measurements for 2015
+# not very nice code regarding date sequences
+# but this way I get everything despite the limit
+# of 1000 measurements per call
 for(month in 9:12){
   dateFrom <- paste0("2015-", month, "-01")
   dateTo <- paste0("2015-", month+1, "-01")
@@ -19,6 +23,10 @@ for(month in 9:12){
                                        date_from=dateFrom,
                                        date_to=dateTo))
 }
+
+# filter negative values
+# and rename columns for compatibilities with openair
+# I do not drop the old columns though
 measurementsNL <- dplyr::mutate(measurementsNL, date=dateLocal,
                                 pm25=value) %>%
   filter(value>=0)
@@ -26,7 +34,9 @@ measurementsNL <- dplyr::mutate(measurementsNL, date=dateLocal,
 # for now openair functions do not work with tbl_df objects
 measurementsNL <- as.data.frame(measurementsNL)
 
+# cool calendar plot
 calendarPlot(mydata=measurementsNL, pollutant = "pm25", year =2015)
 
+# useful timeplot
 timePlot(mydata=measurementsNL, pollutant = "pm25")
 
