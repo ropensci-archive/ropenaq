ef69479e42dbb1c28e81deaca9ef31baee3e5c91
masalmon <maelle.salmon@yahoo.se>
2016-01-29 16:57:08 +0100

    adds a test

--- tests/testthat/test-latest.R
+++ tests/testthat/test-latest.R
@@ -9,6 +9,35 @@ test_that("latest returns a data.frame (tbl_df)", {
 
 })
 
+test_that("latest has the right columns", {
+  skip_on_cran()
+  tableRes <- latest()
+  expect_true(all(names(tableRes) == c("location",
+                                       "city",
+                                       "country",
+                                       "longitude",
+                                       "latitude",
+                                       "parameter",
+                                        "value",
+                                       "lastUpdated",
+                                       "unit",
+                                       "cityURL",
+                                       "locationURL")))
+  classCol <- unlist(lapply(tableRes, class))
+  expect_true(all(classCol==c("character",
+                             "character",
+                             "character",
+                             "numeric",
+                             "numeric",
+                             "character",
+                             "numeric",
+                             "POSIXct",
+                             "POSIXt",
+                             "character",
+                             "character",
+                             "character")))
+})
+
 # test_that("The value_from and value_to arguments work as they should", {
 #   skip_on_cran()
 #   expect_true(all(latest(value_from=10)$value>=10), TRUE)
