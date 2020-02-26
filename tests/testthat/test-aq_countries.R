54b847ecc9203d953b806ff1420822afd9fc4ba1
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-02-26 11:08:03 +0100

    aq_countries tests

--- tests/testthat/test-aq_countries.R
+++ tests/testthat/test-aq_countries.R
@@ -1,10 +1,9 @@
-library("ropenaq")
-#################################################################################################
-context("countries")
-#################################################################################################
 test_that("countries returns a data.frame (tbl_df) with attributes", {
-  skip_on_cran()
-  output <- aq_countries()
+
+  vcr::use_cassette("aq_countries", {
+    output <- aq_countries()
+  })
+
   expect_that(output, is_a("tbl_df"))
   expect_that(attr(output, "meta"), is_a("tbl_df"))
   expect_that(attr(output, "timestamp"), is_a("tbl_df"))
@@ -12,12 +11,14 @@ test_that("countries returns a data.frame (tbl_df) with attributes", {
 
 
 test_that("countries has the right columns", {
-  skip_on_cran()
-  output <- aq_countries()
-  tableRes <- output
-  expect_true(class(tableRes$count) == "integer")
-  expect_true(class(tableRes$code) == "character")
-  expect_true(class(tableRes$name) == "character")
+
+  vcr::use_cassette("aq_countries", {
+    output <- aq_countries()
+  })
+
+  expect_true(class(output$count) == "integer")
+  expect_true(class(output$code) == "character")
+  expect_true(class(output$name) == "character")
   meta <- attr(output, "meta")
   expect_true(all(names(meta) == c("name", "license",
                                    "website", "page",
