2b54e443b3f67bd9ab94daf9106d706e166f4964
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-02-26 16:37:02 +0100

    ?

--- tests/testthat/test-error-handling.R
+++ tests/testthat/test-error-handling.R
@@ -1,7 +1,7 @@
 test_that("error handling", {
   webmockr::enable()
   stub <- webmockr::stub_request("get", paste0(ropenaq:::base_url(), "latest"))
-  webmockr::to_return(stub, status = 429)
+  webmockr::to_raise(stub, fauxpas::HTTPTooManyRequests)
 
     expect_message(
       output <- aq_latest(),
